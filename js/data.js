72756e503f983c4c3754dc6e10771479bdf41894
Bluesman <dmitry.khalimov@gmail.com>
2020-06-19 12:39:33 +0300

    Выделена функция генерации массива данных

--- js/data.js
+++ js/data.js
@@ -3,6 +3,94 @@
 (function () {
   var ADS_QUANTITY = 8;
 
+  var AVATAR_NAME_PREFIX = 'img/avatars/user';
+  var AVATAR_NAME_POSTFIX = '.png';
+
+  var TITLE = [
+    'Большая уютная квартира',
+    'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец',
+    'Маленький ужасный дворец',
+    'Красивый гостевой домик',
+    'Некрасивый негостеприимный домик',
+    'Уютное бунгало далеко от моря',
+    'Неуютное бунгало по колено в воде'
+  ];
+
+  var APPARTMENT_TYPE = [
+    'palace',
+    'flat',
+    'house',
+    'bungalo'
+  ];
+
+  var APPARTMENT_TIME = [
+    '12:00',
+    '13:00',
+    '14:00',
+  ];
+
+  var APPARTMENT_PICTURES = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ];
+
+  var APPARTMENT_FEATURES = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner'
+  ];
+
+  var mainArray = [];
+
+  var createPoint = function (index) {
+    var location = {
+      x: window.utils.generateRandomNumber(0, 1200),
+      y: window.utils.generateRandomNumber(130, 630)
+    };
+    return {
+      author: {
+        avatar: generateAvatarLink(index)
+      },
+      offer: {
+        title: TITLE[index],
+        address: location.x + ', ' + location.y,
+        price: window.utils.generateRandomNumber(1000, 100000),
+        type: APPARTMENT_TYPE[window.utils.generateRandomNumber(0, 3)],
+        rooms: window.utils.generateRandomNumber(1, 5),
+        guests: window.utils.generateRandomNumber(1, 8),
+        checkin: APPARTMENT_TIME[window.utils.generateRandomNumber(0, 2)],
+        checkout: APPARTMENT_TIME[window.utils.generateRandomNumber(0, 2)],
+        features: generateFeatures(),
+        description: '',
+        pictures: generatePictures()
+      },
+      location: location
+    };
+  };
+
+  var generateFeatures = function () {
+    var outputArray = window.utils.mixArray(APPARTMENT_FEATURES);
+    var numberFeatures = window.utils.generateRandomNumber(1, APPARTMENT_FEATURES.length - 1);
+
+    while (outputArray.length > numberFeatures) {
+      outputArray.pop();
+    }
+    return outputArray;
+  };
+
+  var generateAvatarLink = function (number) {
+    return AVATAR_NAME_PREFIX + '0' + (number + 1) + AVATAR_NAME_POSTFIX;
+  };
+
+  var generatePictures = function () {
+    return window.utils.mixArray(APPARTMENT_PICTURES);
+  };
+
   window.data = {
     fillArray: function () {
       for (var i = 0; i < ADS_QUANTITY; i++) {
@@ -10,4 +98,5 @@
       }
     }
   };
+  window.mainArray = mainArray;
 })();
--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
 'use strict';
 
-var ADS_QUANTITY = 8;
-
-var AVATAR_NAME_PREFIX = 'img/avatars/user';
-var AVATAR_NAME_POSTFIX = '.png';
-
-var TITLE = [
-  'Большая уютная квартира',
-  'Маленькая неуютная квартира',
-  'Огромный прекрасный дворец',
-  'Маленький ужасный дворец',
-  'Красивый гостевой домик',
-  'Некрасивый негостеприимный домик',
-  'Уютное бунгало далеко от моря',
-  'Неуютное бунгало по колено в воде'
-];
-
-var APPARTMENT_TYPE = [
-  'palace',
-  'flat',
-  'house',
-  'bungalo'
-];
-
 var TYPE_FLAT = {
   'palace': 'Дворец',
   'flat': 'Квартира',
@@ -37,33 +14,12 @@ var TYPE_FLAT_MIN_VALUE = {
   'bungalo': 0
 };
 
-var APPARTMENT_TIME = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-var APPARTMENT_FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner'
-];
-
 var DISABLED_ROOMS = {
   '1': ['1'],
   '2': ['1', '2'],
   '3': ['1', '2', '3'],
   '100': ['0']
 };
-
-var APPARTMENT_PICTURES = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-];
 
 var PIN_SHIFT_X = 25;
 var PIN_SHIFT_Y = 70;
@@ -75,59 +31,7 @@ var MAP_PIN_HEIGHT = 62;
 var MAP_PIN_TICK_HEIGHT = 22;
 var MAP_PIN_TICK_TOP_SHIFT = -5;
 
-var mainArray = [];
-
-var generateAvatarLink = function (number) {
-  return AVATAR_NAME_PREFIX + '0' + (number + 1) + AVATAR_NAME_POSTFIX;
-};
-
-var generatePictures = function () {
-  return window.utils.mixArray(APPARTMENT_PICTURES);
-};
-
-var generateFeatures = function () {
-  var outputArray = window.utils.mixArray(APPARTMENT_FEATURES);
-  var numberFeatures = window.utils.generateRandomNumber(1, APPARTMENT_FEATURES.length - 1);
-
-  while (outputArray.length > numberFeatures) {
-    outputArray.pop();
-  }
-  return outputArray;
-};
-
-var fillArray = function () {
-  for (var i = 0; i < ADS_QUANTITY; i++) {
-    mainArray.push(createPoint(i));
-  }
-};
-
-var createPoint = function (index) {
-  var location = {
-    x: window.utils.generateRandomNumber(0, 1200),
-    y: window.utils.generateRandomNumber(130, 630)
-  };
-  return {
-    author: {
-      avatar: generateAvatarLink(index)
-    },
-    offer: {
-      title: TITLE[index],
-      address: location.x + ', ' + location.y,
-      price: window.utils.generateRandomNumber(1000, 100000),
-      type: APPARTMENT_TYPE[window.utils.generateRandomNumber(0, 3)],
-      rooms: window.utils.generateRandomNumber(1, 5),
-      guests: window.utils.generateRandomNumber(1, 8),
-      checkin: APPARTMENT_TIME[window.utils.generateRandomNumber(0, 2)],
-      checkout: APPARTMENT_TIME[window.utils.generateRandomNumber(0, 2)],
-      features: generateFeatures(),
-      description: '',
-      pictures: generatePictures()
-    },
-    location: location
-  };
-};
-
-fillArray();
+window.data.fillArray();
 
 var changeInputs = function (inputClass, isDisabled) {
   var inputs = document.querySelectorAll(inputClass);
@@ -156,8 +60,8 @@ var renderAdvert = function (advert, numberCard) {
 var fragment = document.createDocumentFragment();
 
 var generateFragment = function () {
-  for (var i = 0; i < mainArray.length; i++) {
-    fragment.appendChild(renderAdvert(mainArray[i], i));
+  for (var i = 0; i < window.mainArray.length; i++) {
+    fragment.appendChild(renderAdvert(window.mainArray[i], i));
   }
 };
 
@@ -293,7 +197,7 @@ var placeCard = function (numberCard) {
   }
 
   var fragmentCard = document.createDocumentFragment();
-  fragmentCard.appendChild(renderCard(mainArray[numberCard]));
+  fragmentCard.appendChild(renderCard(window.mainArray[numberCard]));
   document.querySelector('.map').insertBefore(fragmentCard, document.querySelector('.map__filters-container'));
 
   document.addEventListener('keydown', onCardEsc);
