a0d8987e7b6e89cef43c17f7a2077a11c90eeb5d
Bluesman <dmitry.khalimov@gmail.com>
2020-06-25 00:29:21 +0300

    По-царски зарефакторил форму + обработчик на кнопку очистить

--- js/form.js
+++ js/form.js
@@ -64,66 +64,59 @@
 
   updateMapAddress(window.map.MAP_PIN_DEFAULT_X + window.map.MAP_PIN_WIDTH / 2, window.map.MAP_PIN_DEFAULT_Y + window.map.MAP_PIN_HEIGHT / 2);
 
-  var successTemplate = document.querySelector('#success').content.querySelector('.success');
-  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var main = document.querySelector('main');
 
-  var onError = function () {
-    var errorPopup = errorTemplate.cloneNode(true);
+  var createPopup = function (nameFunction) {
+    var popupTemplate = document.querySelector('#' + nameFunction).content.querySelector('.' + nameFunction);
+    var popup = popupTemplate.cloneNode(true);
 
-    errorPopup.addEventListener('click', onErrorWindowClick);
-    document.addEventListener('keydown', onErrorWindowEsc);
-    window.drawPopups.openPopup(errorPopup);
+    var closePopup = function () {
+      document.querySelector('.' + nameFunction).removeEventListener('click', onPopupClick);
+      document.removeEventListener('keydown', onPopupEsc);
+      document.querySelector('.' + nameFunction).remove();
+    };
+
+    var onPopupClick = function () {
+      closePopup();
+    };
+
+    var onPopupEsc = function (evt) {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        closePopup();
+      }
+    };
+
+    popup.addEventListener('click', onPopupClick);
+    document.addEventListener('keydown', onPopupEsc);
+
+    main.appendChild(popup);
   };
 
-  var onErrorWindowClick = function () {
-    closeErrorWindow();
-  };
+  var formAdvert = document.querySelector('.ad-form');
 
-  var closeErrorWindow = function () {
-    document.querySelector('.error').removeEventListener('click', onErrorWindowClick);
-    document.removeEventListener('keydown', onErrorWindowEsc);
-    window.drawPopups.closePopup(document.querySelector('.error'));
-  };
-
-  var onErrorWindowEsc = function (evt) {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      closeErrorWindow();
-    }
-  };
-
-  var onSend = function () {
-    var successPopup = successTemplate.cloneNode(true);
-
-    successPopup.addEventListener('click', onSuccessWindowClick);
-    document.addEventListener('keydown', onSuccessWindowEsc);
-
-    window.drawPopups.openPopup(successPopup);
-
-    resetButton.click();
+  var fullReset = function () {
+    formAdvert.reset();
     window.map.deactivateMap();
     window.pin.removeSimilar();
     window.pin.reloadData();
   };
 
-  var onSuccessWindowClick = function () {
-    closeSuccessWindow();
+  resetButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    fullReset();
+  });
+
+  var onSend = function () {
+    createPopup('success');
+    fullReset();
   };
 
-  var onSuccessWindowEsc = function (evt) {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      closeSuccessWindow();
-    }
+  var onError = function () {
+    createPopup('error');
   };
 
-  var closeSuccessWindow = function () {
-    document.querySelector('.success').removeEventListener('click', onSuccessWindowClick);
-    document.removeEventListener('keydown', onSuccessWindowEsc);
-    window.drawPopups.closePopup(document.querySelector('.success'));
-  };
 
-  var formAdvert = document.querySelector('.ad-form');
   var onFormSubmit = function (evt) {
     window.backend.sendLoadData('POST', 'https://javascript.pages.academy/keksobooking', onSend, onError, new FormData(formAdvert));
     evt.preventDefault();
