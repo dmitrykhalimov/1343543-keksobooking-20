03a8342b98c1dfab7d618747d9437745f7a0a9a9
Bluesman <dmitry.khalimov@gmail.com>
2020-07-01 12:10:58 +0300

    Мегарефакторинг

--- js/filters.js
+++ js/filters.js
@@ -1,7 +1,6 @@
 'use strict';
 
 (function () {
-
   var prices = [
     {
       limit: 0,
@@ -21,31 +20,17 @@
   ];
 
   var housingType = document.querySelector('#housing-type');
-  var housingPrice = document.querySelector('#housing-price');
   var housingRooms = document.querySelector('#housing-rooms');
   var housingGuests = document.querySelector('#housing-guests');
+  var housingPrice = document.querySelector('#housing-price');
   var housingFeatures = document.querySelector('#housing-features');
   var features = housingFeatures.querySelectorAll('.map__checkbox');
 
   var filtersForm = document.querySelector('.map__filters');
   var filteredSimilarPins = window.pin.mainArray;
-  var filtersMap;
 
-  var buildFeaturesArray = function () {
-    var checkedFeatures = [];
-    for (var i = 0; i < features.length; i++) {
-      if (features[i].checked) {
-        checkedFeatures.push(features[i].value);
-      }
-    }
-    return checkedFeatures;
-  };
-
-  var translateAny = function (valueToTranslate) {
-    if (valueToTranslate === 'any') {
-      return -1;
-    }
-    return valueToTranslate;
+  var checkSimilar = function (element, offer) {
+    return element.value === 'any' || element.value === offer;
   };
 
   var translatePriceToText = function (valueToTranslate) {
@@ -54,54 +39,25 @@
         return prices[i].name;
       }
     }
-    return -1;
+    return 'any';
   };
 
-  var buildFiltersMap = function () {
-    filtersMap = {};
-    filtersMap.offer = {};
-    filtersMap.offer.type = translateAny(housingType.value);
-    filtersMap.offer.price = translateAny(housingPrice.value);
-    filtersMap.offer.rooms = Number(translateAny(housingRooms.value));
-    filtersMap.offer.guests = Number(translateAny(housingGuests.value));
-    filtersMap.offer.features = buildFeaturesArray();
-  };
-
-  var returnFlag = 0;
-
-  var checkSimilar = function (similarPin, field) {
-    switch (field) {
-      case 'type':
-      case 'rooms':
-      case 'guests': {
-        if ((similarPin.offer[field] === filtersMap.offer[field]) || filtersMap.offer[field] === -1) {
-          returnFlag++;
-        }
-        break;
-      }
-      case 'price': {
-        if ((translatePriceToText(similarPin.offer[field]) === filtersMap.offer[field]) || filtersMap.offer[field] === -1) {
-          returnFlag++;
-        }
-        break;
-      }
-      case 'features': {
-        if (window.utils.isIncludeArray(similarPin.offer[field], filtersMap.offer[field])) {
-          returnFlag++;
-        }
-        break;
+  var checkFeatures = function (offer) {
+    var checkedFeatures = [];
+    for (var i = 0; i < features.length; i++) {
+      if (features[i].checked) {
+        checkedFeatures.push(features[i].value);
       }
     }
+    return !checkedFeatures.length || window.utils.isIncludeArray(offer.features, checkedFeatures);
   };
 
-  var isSimilar = function (similarPin) {
-    returnFlag = 0;
-    checkSimilar(similarPin, 'type');
-    checkSimilar(similarPin, 'rooms');
-    checkSimilar(similarPin, 'price');
-    checkSimilar(similarPin, 'guests');
-    checkSimilar(similarPin, 'features');
-    return (returnFlag === 5);
+  var isSimilar = function (offer) {
+    return checkSimilar(housingType, offer.offer['type']) &&
+    checkSimilar(housingRooms, String(offer.offer['rooms'])) &&
+    checkSimilar(housingPrice, translatePriceToText(offer.offer['price'])) &&
+    checkSimilar(housingGuests, offer.offer['guests']) &&
+    checkFeatures(offer.offer);
   };
 
   var onFiltersBarChange = function () {
@@ -110,15 +66,12 @@
       if (document.querySelector('.popup')) {
         window.placeCard.closeCard();
       }
-      buildFiltersMap();
-      var counter = 0;
       for (var i = 0; i < window.pin.mainArray.length; i++) {
-        if (counter === window.pin.MAX_PIN_QUANTITY) {
+        if (filteredSimilarPins.length === window.pin.MAX_PIN_QUANTITY) {
           break;
         }
         if (isSimilar(window.pin.mainArray[i])) {
           filteredSimilarPins.push(window.pin.mainArray[i]);
-          counter++;
         }
       }
       window.pin.reloadData(filteredSimilarPins);
